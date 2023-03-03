36a458dcd85b820523dab5a1a8fe74f0f776938f
scicco <zendar79@gmail.com>
2023-03-03 17:19:10 +0100

    setup script disabled

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -5,6 +5,7 @@ export MANAGE_LOCAL_SOLR=true
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
 
 /opt/atlas/bin/atlas_start.py
-sh /tmp/atlas-setup.sh
+# we don't need any atlas setup
+#sh /tmp/atlas-setup.sh
 
 tail -f /dev/null
